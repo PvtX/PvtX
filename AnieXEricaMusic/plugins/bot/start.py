9e53a9f998a8eb1002823096c5bc9ac895056cf4
AlonePvtBot <132041403+AloneXBot@users.noreply.github.com>
2024-05-17 04:09:19 +0530

    Update start.py

--- AnieXEricaMusic/plugins/bot/start.py
+++ AnieXEricaMusic/plugins/bot/start.py
@@ -84,7 +84,7 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_sticker("CAACAgQAAxkBAAEGNCdlmOVoVdzYjXDhFD-RBU4NEfAmGQACLwwAAuqTAVKXwqXcqn7CKDQE")
+        await message.reply_sticker("CAACAgUAAxkBAAEKJ05mRoqy1nAqrtPU3zug4a5qFhPWPwACAwwAAv5IIFd_SHjDg6Zx9DUE")
         await message.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
@@ -102,7 +102,7 @@ async def start_pm(client, message: Message, _):
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    await message.reply_sticker("CAACAgQAAxkBAAEGNCdlmOVoVdzYjXDhFD-RBU4NEfAmGQACLwwAAuqTAVKXwqXcqn7CKDQE")
+    await message.reply_sticker("CAACAgUAAxkBAAEKJ05mRoqy1nAqrtPU3zug4a5qFhPWPwACAwwAAv5IIFd_SHjDg6Zx9DUE")
     await message.reply_photo(
         photo=config.START_IMG_URL,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
@@ -138,7 +138,7 @@ async def welcome(client, message: Message):
                     return await app.leave_chat(message.chat.id)
 
                 out = start_panel(_)
-                await message.reply_sticker("CAACAgQAAxkBAAEGNCdlmOVoVdzYjXDhFD-RBU4NEfAmGQACLwwAAuqTAVKXwqXcqn7CKDQE")
+                await message.reply_sticker("CAACAgUAAxkBAAEKJ05mRoqy1nAqrtPU3zug4a5qFhPWPwACAwwAAv5IIFd_SHjDg6Zx9DUE")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_3"].format(
