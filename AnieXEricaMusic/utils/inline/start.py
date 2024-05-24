a002aa759ce8fc0e5f6976da04acabfbe30f8165
AlonePvtBot <132041403+AloneXBot@users.noreply.github.com>
2024-05-24 10:27:54 +0530

    Update start.py

--- AnieXEricaMusic/utils/inline/start.py
+++ AnieXEricaMusic/utils/inline/start.py
@@ -29,17 +29,8 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(
-                text=_["S_B_7"],
-                url=f"https://github.com/TeamAloneOp/AloneXMusic",
-            )
-        ],
-        [InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG")],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
