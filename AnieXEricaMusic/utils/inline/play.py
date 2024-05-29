d6b543827618661e98ba5e7b091d1a6712216588
AlonePvtBot <132041403+AloneXBot@users.noreply.github.com>
2024-05-29 10:22:00 +0530

    Add files via upload

--- AnieXEricaMusic/utils/inline/play.py
+++ AnieXEricaMusic/utils/inline/play.py
@@ -61,8 +61,12 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
 
         [
+            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="✚", callback_data=f"Ambot_playlist {videoid}"
+            ),
         ],
 
         
@@ -76,11 +80,15 @@ def stream_markup(_, chat_id):
         
 
         [
+            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="✚", callback_data=f"Ambot_playlist {videoid}"
+            ),
         ],
 
-    
+        
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
