24ff68f7f9946f6a5452b3c9dab99ff3295dd629
sᴀɢᴀʀ ᴛɪᴡᴀʀɪ <112709751+stkeditz@users.noreply.github.com>
2024-06-06 17:21:25 +0530

    Update start.py

--- AnieXEricaMusic/plugins/bot/start.py
+++ AnieXEricaMusic/plugins/bot/start.py
@@ -94,7 +94,6 @@ async def start_pm(client, message: Message, _):
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
-            protect_content=True,
         )
         if await is_on_off(2):
             return await app.send_message(
