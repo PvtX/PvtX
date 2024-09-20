766973b71549717fe6b989c76f036126ffa7486f
AlonePvtBot <132041403+AloneXBot@users.noreply.github.com>
2024-09-20 21:52:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID","6435225"))
 API_HASH = getenv("API_HASH","4e984ea35f854762dcde906dce426c2d")
-BOT_TOKEN = getenv("BOT_TOKEN","5963562690:AAFBfHpP6wcx_xgPJiChfMvp_VBK9rBvFo8")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://public:abishnoimf@cluster0.rqk6ihd.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN","5963562690:AAG2iwTRSTWkd1tyKFkbZWjSBrClc8nmXT0")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://hny:zara@cluster0.lfe5o.mongodb.net/?retryWrites=true&w=majority")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "9999"))
 LOGGER_ID = int(getenv("LOGGER_ID", "-1001603822916"))
 BOTADDLOGS = int(getenv("BOTADDLOGS", "-1001775986475")) # LOGGER_ID Id Also Use No Problem
